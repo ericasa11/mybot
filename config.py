f855d99fcb57bcaaca4c5046f04a13b924152914
ericasa11 <159577590+ericasa11@users.noreply.github.com>
2024-02-10 03:10:10 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+# API_HASH = os.getenv("API_HASH")
+# API_ID = int(os.getenv("API_ID"))
+# HEROKU_API = os.getenv("HEROKU_API")
+# HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+# PY_SESSION = os.getenv("PYROGRAM_SESSION")
+# TE_SESSION = os.getenv("TELETHON_SESSION")
+PREFIX = os.environ.get("PREFIX", ".")
+# LOG_CHAT = int(os.getenv("LOG_CHAT"))
+# X_SESSION = os.getenv("X_SESSION")
+# te_session_strings = [
+#     "app1",
+#     "app2",
+#     "app3",
+#     "app4",
+#     "app5",
+#     "app6",
+
+# ]
