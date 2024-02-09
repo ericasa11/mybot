de0e15ed665e263e4d385d189c760e1fe249c32c
ericasa11 <159577590+ericasa11@users.noreply.github.com>
2024-02-10 02:40:47 +0700

    Create __main__.py

---
+++ _pyrogram/__main__.py
@@ -0,0 +1,9 @@
+# (C) 2021 Jayant Kageri
+
+from pyrogram import idle, Client, filters
+from _pyrogram import app, LOGGER
+from _pyrogram.modules import *
+import os
+
+print("Pyrogram User Client is Starting... \n(C) 2021 ", end="\n\n")
+app.run()
